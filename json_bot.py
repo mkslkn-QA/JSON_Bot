1388f7060464f80a72fa2a40ae21cf62f604663f
Maksim <144241989+mkslkn-QA@users.noreply.github.com>
2023-10-14 02:00:35 +0300

    Add files via upload

---
+++ json_bot.py
@@ -0,0 +1,39 @@
+from telebot import TeleBot, types
+import json
+
+bot = TeleBot(
+    token='6420197872:AAFJn50A8IunUaq4CqaY3M36pd8VXFKhSxs',  parse_mode='html')
+
+
+@bot.message_handler(commands=['start'])
+def start_command_handler(message: types.Message):
+    bot.send_message(
+        chat_id=message.chat.id,
+        text='Привет! Я умею проверять JSON и форматировать его в красивый текст\nВведи JSON в виде строки:',
+    )
+
+
+@bot.message_handler()
+def message_handler(message: types.Message):
+    try:
+        payload = json.loads(message.text)
+    except json.JSONDecodeError as ex:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=f'При обработке произошла ошибка:\n<code>{str(ex)}</code>'
+        )
+        return
+
+    text = json.dumps(payload, indent=2, sort_keys=True, ensure_ascii=False)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=f'JSON:\n<code>{text}</code>'
+    )
+
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
